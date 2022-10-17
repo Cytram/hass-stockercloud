66456dc627c2c15500ace9581eddd9c5cae84b25
Jesper Petersen <jesper.petersen@patientsky.com>
2022-10-17 17:43:01 +0200

    Rename

--- custom_components/intergration_stockercloud/const.py
+++ custom_components/intergration_stockercloud/const.py
@@ -1,11 +1,11 @@
-"""Constants for integration_blueprint."""
+"""Constants for integration_stockercloud."""
 # Base component constants
 NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+DOMAIN = "integration_stockercloud"
 DOMAIN_DATA = f"{DOMAIN}_data"
 VERSION = "0.0.1"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
-ISSUE_URL = "https://github.com/custom-components/integration_blueprint/issues"
+ISSUE_URL = "https://github.com/custom-components/integration_stockercloud/issues"
 
 # Icons
 ICON = "mdi:format-quote-close"
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,1 @@
-"""Tests for integration_blueprint integration."""
+"""Tests for integration_stockercloud integration."""
